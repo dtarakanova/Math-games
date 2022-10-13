8c12544f61acbb635e77f25e8253e5b1c9f92036
dariatarakanova <dshargalina@gmail.com>
2022-10-13 17:28:22 +0300

    Bug fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,12 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        System.out.println("Welcome to the Brain Games!\nMay I have your name?");
+        Scanner sc = new Scanner(System.in);
+        String userName = sc.next();
+        System.out.println("Hello, " + userName + "!");
     }
 }
