4bd2e5ba2bd566550b08654459a4fbbd89a7115b
dariatarakanova <dshargalina@gmail.com>
2022-10-17 17:17:08 +0300

    Add Cli.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,15 +1,11 @@
 package hexlet.code;
 
+import src.main.java.hexlet.code.Cli;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter./n
-                1 - Greet
-                0 - Exit
-                Your choice:");
-        Scanner sc = new Scanner(System.in);
-        String userName = sc.next();
-        System.out.println("Hello, " + userName + "!");
+        Cli.greeting();
     }
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,12 @@
+package src.main.java.hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greeting() {
+        System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
+        Scanner sc = new Scanner(System.in);
+        String userName = sc.next();
+        System.out.println("Hello, " + userName + "!");
+    }
+}
