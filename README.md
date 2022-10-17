89f224d87dc60e468d4be273b9b66ad4b41b1ce7
dariatarakanova <dshargalina@gmail.com>
2022-10-17 16:49:40 +0300

    Add Codeclimate

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/dtarakanova/java-project-61/workflows/hexlet-check/badge.svg)](https://github.com/dtarakanova/java-project-61/actions)
 
-asciinema
+<a href="https://codeclimate.com/github/dtarakanova/java-project-61/maintainability"><img src="https://api.codeclimate.com/v1/badges/0cd5d6933ba48ee3a601/maintainability" /></a>
+
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!\nMay I have your name?");
+        System.out.println("Please enter the game number and press Enter./n
+                1 - Greet
+                0 - Exit
+                Your choice:");
         Scanner sc = new Scanner(System.in);
         String userName = sc.next();
         System.out.println("Hello, " + userName + "!");
