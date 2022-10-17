eff7d964abacac76d565f5872d5a221c7e30960b
dariatarakanova <dshargalina@gmail.com>
2022-10-17 17:32:58 +0300

    Update App.java logic

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,20 @@
 package hexlet.code;
 
-import src.main.java.hexlet.code.Cli;
-
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        System.out.print("Please enter the game number and press Enter.\n" +
+                "1 - Greet\n" +
+                "0 - Exit\n" +
+                "Your choice: ");
+        Scanner gn = new Scanner(System.in);
+        String gameNumber = gn.next();
+        System.out.println();
+
+        System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
+        Scanner sc = new Scanner(System.in);
+        String userName = sc.next();
+        System.out.println("Hello, " + userName + "!");
     }
 }
---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,8 @@
+package src.main.java.hexlet.code;
+
+public class Engine {
+    public static void gameMechanics () {
+
+    }
+
+}
