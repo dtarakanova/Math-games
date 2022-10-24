60d9f68fede7bb169f1ecf4bb8204985fe8ebfcb
dariatarakanova <dshargalina@gmail.com>
2022-10-24 17:40:06 +0300

    Remove static fields from Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,44 +3,38 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Engine {
-    private static String userName;
-    private static String answer;
     private static final int CORRECTANSWERS = 3;
 
     //Механика игры
     public static void gameMechanics(String gameTask, String[] question, String[] rightAnswer) {
         var i = 0;
-        greeting();
+        String userName;
+        String answer;
+
+        //Приветствие и ввод имени
+        System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
+        Scanner sc = new Scanner(System.in);
+        userName = sc.next();
+        System.out.println("Hello, " + userName + "!");
+
+        //Запуск механики игры
         System.out.println(gameTask);
         for (i = 0; i < CORRECTANSWERS; i++) {
             System.out.println(question[i]);
-            getAnswer();
+
+            //Получение ответа пользователя
+            Scanner ta = new Scanner(System.in);
+            System.out.print("Your answer: ");
+            answer = ta.next();
+
             if (answer.equals(rightAnswer[i])) {
                 System.out.println("Correct!");
             } else {
                 System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + rightAnswer[i] + "'.");
                 System.out.println("Let's try again, " + userName + "!");
-                break;
+                return;
             }
         }
-        if (i == CORRECTANSWERS) {
-            System.out.println("Congratulations, " + userName + "!");
-        }
+        System.out.println("Congratulations, " + userName + "!");
     }
-
-    //Приветствие и ввод имени
-    private static void greeting() {
-        System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
-        Scanner sc = new Scanner(System.in);
-        userName = sc.next();
-        System.out.println("Hello, " + userName + "!");
-    }
-
-    //Получение ответа пользователя
-    private static void getAnswer() {
-        Scanner ta = new Scanner(System.in);
-        System.out.print("Your answer: ");
-        answer = ta.next();
-    }
-
 }
