dfca12ac2bc9722744809f3606dccbf1326065c3
dariatarakanova <dshargalina@gmail.com>
2022-10-25 11:28:46 +0300

    Code refactoring

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -21,44 +21,29 @@ public class App {
 
 
     public static void main(String[] args) {
-        System.out.print("Please enter the game number and press Enter.\n"
-                + "1 - Greet\n"
-                + "2 - Even\n"
-                + "3 - Calc\n"
-                + "4 - GCD\n"
-                + "5 - Progression\n"
-                + "6 - Prime\n"
-                + "0 - Exit\n"
-                + "Your choice: ");
+        System.out.print("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                3 - Calc
+                4 - GCD
+                5 - Progression
+                6 - Prime
+                0 - Exit
+                Your choice:\s""");
         Scanner gn = new Scanner(System.in);
         int gameNumber = Integer.parseInt(gn.next());
         System.out.println();
 
         switch (gameNumber) {
-            case GOODBYE:
-                System.out.println("Goodbye!");
-                break;
-            case GREETING:
-                Cli.greeting();
-                break;
-            case EVENGAME:
-                Even.evenGame();
-                break;
-            case CALCGAME:
-                Calc.calculationGame();
-                break;
-            case GCDGAME:
-                GCD.findGCD();
-                break;
-            case PROGRESSIONGAME:
-                Progression.generateProgression();
-                break;
-            case PRIMEGAME:
-                Prime.primeGame();
-                break;
-            default:
-                System.out.println("No game " + gameNumber + " found");
-                break;
+            case GOODBYE -> System.out.println("Goodbye!");
+            case GREETING -> Cli.greeting();
+            case EVENGAME -> Even.evenGame();
+            case CALCGAME -> Calc.calculationGame();
+            case GCDGAME -> GCD.findGCD();
+            case PROGRESSIONGAME -> Progression.generateProgression();
+            case PRIMEGAME -> Prime.primeGame();
+            default -> System.out.println("No game " + gameNumber + " found");
         }
     }
 }
